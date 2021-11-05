ca998327d5ebfbd0044429f5b12105e3f64ceccb
Robert <rstrube@gmail.com>
2021-11-05 13:24:39 -0600

    Fixed ordering for papirus icon ingredients

--- ingredients/gnome-ext/tray-icons.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#|#./ingredients/gnome-ext/tray-icons.sh #Tray Icons Reloaded Gnome Extension https://github.com/MartinPL/Tray-Icons-Reloaded
-
-paru -S --noconfirm --needed gnome-shell-extension-tray-icons-reloaded-git
