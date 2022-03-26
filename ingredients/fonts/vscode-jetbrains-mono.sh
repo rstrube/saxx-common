819509c83a6feb054eedd3aa748f0d92af22bdb1
Robert <rstrube@gmail.com>
2022-03-25 20:44:27 -0600

    Fixed a pathing issues in ingredient

--- ingredients/fonts/vscode-jetbrains-mono.sh
+++ ingredients/fonts/vscode-jetbrains-mono.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#|#./ingredients/dev/vscode-jetbrains-mono.sh #JetBrains Mono font for Visual Studio Code [Requires /dev/1_vscode and /fonts/0_xxx-fonts ingredients]
+#|#./ingredients/fonts/vscode-jetbrains-mono.sh #JetBrains Mono font for Visual Studio Code [Requires /dev/1_vscode and /fonts/0_xxx-fonts ingredients]
 
 sed -i '$i\    "editor.fontFamily": "JetBrains Mono",' "$VSCODE_SETTINGS_FILE_PATH"
 sed -i '$i\    "editor.fontWeight": "500",' "$VSCODE_SETTINGS_FILE_PATH"
