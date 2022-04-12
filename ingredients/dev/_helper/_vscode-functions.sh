e207d9972a0368df1dfe709223423b6ba9b4ca37
Robert <rstrube@gmail.com>
2022-04-12 16:18:47 -0600

    Updated vscode ingredients

--- ingredients/dev/_helper/_vscode-functions.sh
+++ ingredients/dev/_helper/_vscode-functions.sh
@@ -18,6 +18,17 @@ function create_empty_vscode_settings_if_neccessary {
     fi
 }
 
+function update_desktop_file_for_wayland {
+
+    # Copy the system .desktop file to your $HOME and tweak it to launch VSCode as a native Wayland application
+    if [[ ! -d "~/.local/share/applications" ]]; then
+        mkdir -p ~/.local/share/applications
+    fi
+
+    cp /usr/share/applications/visual-studio-code.desktop ~/.local/share/applications/.
+    sed -i 's/\/usr\/bin\/code/& --enable-features=UseOzonePlatform --ozone-platform=wayland/' ~/.local/share/applications/visual-studio-code.desktop
+}
+
 function create_and_configure_code_flags_for_wayland {
 
     cat <<EOT > "code-flags.conf"	
