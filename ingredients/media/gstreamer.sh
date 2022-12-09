8076070d47cff9687257918bcc1e5efde6f7ac1a
Robert <rstrube@gmail.com>
2022-12-08 19:48:31 -0700

    Moved GStreamer ingredients into core installation scripts.  Updated .NET ingredients to use arch packages rather than AUR.  Fixed some issues with .NET HTTPS certificate.  Removed obsolete ingredients.

--- ingredients/media/gstreamer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#|#./ingredients/media/gstreamer.sh #Additional GStreamer plugins
-
-paru -S --noconfirm --needed gst-libav gst-plugins-good gst-plugins-bad gst-plugins-ugly gstreamer-vaapi gst-plugin-pipewire
