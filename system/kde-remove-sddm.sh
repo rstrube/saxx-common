06c14095a5e4572683bfbb44c8961f75e21292fc
Robert <rstrube@gmail.com>
2023-05-15 15:40:22 -0600

    Cleaned up the SSDM removal ingredient

--- system/kde-remove-sddm.sh
+++ system/kde-remove-sddm.sh
@@ -10,6 +10,7 @@ paru -Rns sddm sddm-kcm
 BASH_PROFILE=~/.bash_profile
 FISH_CONFIG_DIR=~/.config/fish/conf.d
 TTY1_PLASMA_START_FISH_FILE=tty1-plasma-start.fish
+PAM_LOGIN_FILE=/etc/pam.d/login
 
 # Append to .bash_profile in order to start KDE plasma when logging into to TTY1
 echo "Appending to ${BASH_PROFILE} to support starting plasma after login on TTY1."
@@ -21,7 +22,7 @@ echo '[ "$(tty)" = "/dev/tty1" ] && exec startplasma-wayland' >> $BASH_PROFILE
 # If using fish, setup a .fish file to start KDE plasma with logging into TTY1
 if [[ -e /usr/bin/fish ]]; then
 
-    echo "fish detected, creating ~/.config/fish/conf.d/plasma.fish to support starting plasma after login on TTY1."
+    echo "fish detected, creating ${FISH_CONFIG_DIR}/${TTY1_PLASMA_START_FISH_FILE} to support starting plasma after login on TTY1."
 
     cat <<EOT > ${TTY1_PLASMA_START_FISH_FILE}
 # If running from tty1 start kde plasma
@@ -35,5 +36,5 @@ fi
 
 echo "Configuring kwallet to unlock when logging in via TTY1."
 
-echo "auth       optional     pam_kwallet5.so" | sudo tee -a /etc/pam.d/login
-echo "session    optional     pam_kwallet5.so auto_start force_run" | sudo tee -a /etc/pam.d/login
+echo "auth       optional     pam_kwallet5.so" | sudo tee -a $PAM_LOGIN_FILE
+echo "session    optional     pam_kwallet5.so auto_start force_run" | sudo tee -a $PAM_LOGIN_FILE
