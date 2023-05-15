38ac1a561221218327d40836087270e6d54e84f5
Robert <rstrube@gmail.com>
2023-05-15 14:21:42 -0600

    More cleanups to ingredients

--- dev/1_vscode.sh
+++ dev/1_vscode.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
 #|#./ingredients/dev/1_vscode.sh #Visual Studio Code
 
+CONFIG_DIR=~/.config
+CODE_FLAGS=code-flags.conf
+VSCODE_SETTINGS_DIR_PATH="${CONFIG_DIR}/Code/User"
+VSCODE_SETTINGS_FILE_PATH="${VSCODE_SETTINGS_DIR_PATH}/settings.json"
+
 paru -S --noconfirm --needed visual-studio-code-bin
 
 if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
     echo "Wayland detected, creating ~/.config/code-flags.conf"
     
-    if [[ ! -d "~./config" ]]; then
-        mkdir -p ~/.config
+    if [[ ! -d "$CONFIG_DIR" ]]; then
+        mkdir -p $CONFIG_DIR
     fi
 
     if [[ "$XDG_CURRENT_DESKTOP" == "GNOME" ]]; then
         echo "Gnome detected, adding --enable-features=WaylandWindowDecorations"
 
-        cat <<EOT > "code-flags.conf"
+        cat <<EOT > ${CODE_FLAGS}
 --enable-features=WaylandWindowDecorations
 --enable-webrtc-pipewire-capturer
 --ozone-platform-hint=auto
 EOT
     else
-        cat <<EOT > "code-flags.conf"
+        cat <<EOT > ${CODE_FLAGS}
 --enable-webrtc-pipewire-capturer
 --ozone-platform-hint=auto
 EOT
     fi
 
-    cp code-flags.conf ~/.config/
-    rm code-flags.conf
+    cp $CODE_FLAGS $CONFIG_DIR
+    rm $CODE_FLAGS
 fi
-
-VSCODE_SETTINGS_DIR_PATH="${HOME}/.config/Code/User"
-VSCODE_SETTINGS_FILE_PATH="${VSCODE_SETTINGS_DIR_PATH}/settings.json"
 
 if [[ ! -d "$VSCODE_SETTINGS_DIR_PATH" ]]; then
         mkdir -p "$VSCODE_SETTINGS_DIR_PATH"
--- fonts/jetbrains-mono/jetbrains-mono-vscode.sh
+++ fonts/jetbrains-mono/jetbrains-mono-vscode.sh
@@ -2,7 +2,7 @@
 
 function configure_jetbrains_mono_vscode {
 
-    local VSCODE_SETTINGS_DIR_PATH="${HOME}/.config/Code/User"
+    local VSCODE_SETTINGS_DIR_PATH="~/.config/Code/User"
     local VSCODE_SETTINGS_FILE_PATH="${VSCODE_SETTINGS_DIR_PATH}/settings.json"
 
     sed -i '$i\    "editor.fontFamily": "JetBrains Mono",' "$VSCODE_SETTINGS_FILE_PATH"
