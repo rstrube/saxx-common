5559ae0832fc3610b1841f9e96e5ed20f03e691f
Robert <rstrube@gmail.com>
2025-03-09 21:58:06 -0600

    Updated jetbrains-mono and firefox ingredient

--- fonts/jetbrains-mono.sh
+++ fonts/jetbrains-mono.sh
@@ -1,85 +1,27 @@
 #!/bin/bash
-#|#./ingredients/fonts/jetbrains-mono.sh #Install and configure Jetbrains Mono font
+#|#./ingredients/fonts/jetbrains-mono.sh #Install and configure JetBrainsMono Nerd Font
 
-ETC_FONT_CONFIG_DIR=/etc/fonts/conf.d
-LOCAL_FONT_CONFIG_DIR=~/.config/fontconfig
-FONT_CONFIG_FILE=fonts.conf
+# 2025-03-09
+# ==========
+# * Leverage new arch extras package ttf-jetbrains-mono-nerd which makes things way easier :)
 
 DIR=$(dirname "$0")
-source $DIR/_helper/kitty-font-functions.sh
 source $DIR/_helper/vscode-font-functions.sh
 
-KITTY=/usr/bin/kitty
 VSCODE=/usr/bin/code
 
-# Install the following fonts:
-# Jetbrains Mono: a fantastic monospace font
-# Nerd Fonts: Provides a plethora of font icons
-paru -S --noconfirm --needed ttf-jetbrains-mono ttf-nerd-fonts-symbols
-
-# Create a symlink for the nerd fonts config
-sudo ln -s /usr/share/fontconfig/conf.avail/10-nerd-font-symbols.conf $ETC_FONT_CONFIG_DIR
-
-# Create fonts.conf in $HOME to register JetBrains as a monospace font
-if [[ ! -d "$LOCAL_FONT_CONFIG_DIR" ]]; then
-    mkdir -p $LOCAL_FONT_CONFIG_DIR
-fi
-
-cat <<EOT > ${FONT_CONFIG_FILE}
-<?xml version="1.0"?>
-<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-<fontconfig>
-    <!-- set JetBrains Mono as the preferred monospace font (fallback to Nerd Font for missing glyphs) -->
-    <alias>
-        <family>monospace</family>
-        <prefer>
-            <family>JetBrains Mono</family>
-            <family>Symbols Nerd Font</family>
-        </prefer>
-    </alias>
-    <dir>~/.fonts</dir>
-</fontconfig>
-EOT
-
-mv $FONT_CONFIG_FILE $LOCAL_FONT_CONFIG_DIR
+paru -S --noconfirm --needed ttf-jetbrains-mono-nerd
 
 # This rebuilds the font-cache, taking into account any changes
 sudo fc-cache -r -v
 fc-cache -r -v
 
-# Confirm that Jetbrains Mono is the default font used for monospace
-fc-match --verbose monospace
-
-# KDE
-if [[ "$XDG_CURRENT_DESKTOP" == "KDE" ]]; then
-    echo "Configuring Jetbrains Mono font for KDE"
-
-    kwriteconfig5 --group General --key font "Noto Sans,10,-1,5,50,0,0,0,0,0"
-    kwriteconfig5 --group General --key menuFont "Noto Sans,10,-1,5,50,0,0,0,0,0"
-    kwriteconfig5 --group General --key smallestReadableFont "Noto Sans,8,-1,5,50,0,0,0,0,0"
-    kwriteconfig5 --group General --key fixed "JetBrains Mono,11,-1,5,50,0,0,0,0,0"
-fi
-
-# Gnome / GTK applications
-echo "Configuring Jetbrains Mono font for Gnome / GTK applications"
-
-gsettings set org.gnome.desktop.interface font-name "Noto Sans 10"
-gsettings set org.gnome.desktop.interface document-font-name "Noto Sans 10"
-gsettings set org.gnome.desktop.interface monospace-font-name "JetBrains Mono 11"
-gsettings set org.gnome.desktop.wm.preferences titlebar-font "Noto Sans Medium 10"
-
-# kitty
-if [[ -e "$KITTY" ]]; then
-    echo "Configuring Jetbrains Mono font for kitty"
-
-    configure_kitty_font "JetBrains Mono" "12.0"
-fi
+# Note: you can list fonts using fc-list 
 
 # vscode
 if [[ -e "$VSCODE" ]]; then
-    echo "Configuring Jetbrains Mono font for vscode"
-
-    configure_vscode_font "JetBrains Mono" "500"
+    echo "Configuring JetBrainsMono Nerd Font for vscode"
+    configure_vscode_font "JetBrainsMono Nerd Font" "500"
 fi
 
 # Test font coverage for specific unicode characters required by PowerLine
--- web/firefox.sh
+++ web/firefox.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 #|#./ingredients/web/firefox.sh #Firefox
 
+# 2025-03-09
+# ==========
+# * Firefox >= 121 defaults to Wayland instead of XWayland, no additional configuration required
+
 DIR=$(dirname "$0")
 source $DIR/../_helper/_common-functions.sh
 
-ENVIRONMENT_FILE=/etc/environment
-
 paru -S --noconfirm --needed firefox
-
-if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
-    echo "Wayland detected, setting env var MOZ_ENABLE_WAYLAND=1 in ${ENVIRONMENT_FILE}"
-    echo "MOZ_ENABLE_WAYLAND=1"  | sudo tee -a $ENVIRONMENT_FILE
-fi
 
 echo -e "${YELLOW}Additional about:config tweaks worth considering:${NC}"
 echo -e "${YELLOW}widget.use-xdg-desktop-portal.file-picker=1 : Use native file picker via XDG desktop portal${NC}"
